46b058749c5efe58920c09363cc1a1596103cd3d
Lancinater <zwordgize@gmail.com>
2025-02-13 04:49:36 +1100

    created post schema and tested to connect to database

--- application/app.js
+++ application/app.js
@@ -1,5 +1,23 @@
 let express = require('express');
 let app = express();
+let Post = require('./models/postModel').Post;
+let mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/travels');
+
+let post1 = new Post({
+    id: '1',
+    title: 'Post 1',
+    date: new Date(),
+    description: 'This is the first post',
+    text: 'This is the first post',
+    country: 'USA',
+    imageURL: 'https://via.placeholder.com/150'
+});
+
+post1.save().then(() => {
+    console.log('Post saved');
+});
 
 app.use(express.static('../public'));
 
